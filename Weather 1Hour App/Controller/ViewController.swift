b54dc189bd7115e23f7e6b1198e6e19b08dd21ce
moolnam <moolnam>
2022-05-03 15:10:07 +0900

    지금까지 날씨 API데이터를 받아서 여기까지 했다 이제 할 일은 조건이 충족하면 변하는 색과 1시간 마다 오디오가 나오게 하는 것이다.

--- Weather 1Hour App/Controller/ViewController.swift
+++ Weather 1Hour App/Controller/ViewController.swift
@@ -6,42 +6,106 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
+    
+    var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
     
     @IBOutlet weak var weatherTextField: UITextField!
     @IBOutlet weak var weatherIconImage: UIImageView!
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
         weatherTextField.delegate = self
+        weatherManager.delegate = self
+        
     }
     
     @IBAction func locationButton(_ sender: UIButton) {
-        
+        locationManager.requestLocation()
     }
     
     @IBAction func searchButton(_ sender: UIButton) {
         // Weather Search Button
         weatherTextField.endEditing(true)
+        // 텍스트 필드가 끝났을때 어떻게 처리할지 true
         print(weatherTextField.text!)
         weatherTextField.text = ""
+        // 텍스트 필드 안에 있는 내용을 전무 지운 스트링 타입
     }
     
+    
+    
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(lon: lon, lat: lat)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        print(error)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         weatherTextField.endEditing(true)
+        // 텍스트 필드가 끝났을때 어떻게 처리할지 true
         print(weatherTextField.text!)
         weatherTextField.text = ""
+        // 텍스트 필드 안에 있는 내용을 전무 지운 스트링 타입
         return true
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        return true
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "again"
+            return false
+        }
     }
-
-
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = textField.text {
+            weatherManager.fetchWeather(cityName: city)
+            print(city)
+        }
+        
+        weatherTextField.text = ""
+    }
 }
 
+extension ViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.cityNameLabel.text = weather.cityName
+            self.weatherIconImage.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+    
+}
--- Weather 1Hour App/Model/WeatherManager.swift
+++ Weather 1Hour App/Model/WeatherManager.swift
@@ -6,13 +6,70 @@
 //
 
 import Foundation
+import CoreLocation
+import AVFoundation
+
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
 
 
 struct WeatherManager {
-    let apiKey = "139&appid=27edc121638773d8a67d862806f2d6c9"
+    
+    var delegate: WeatherManagerDelegate?
+    
+    let apiKey = "27edc121638773d8a67d862806f2d6c9"
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?"
     
-    func fethWeather() {
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&appid=\(apiKey)&q=\(cityName)"
+        print(urlString)
+        performRequest(with: urlString)
+    }
+    
+    func fetchWeather(lon: CLLocationDegrees, lat: CLLocationDegrees) {
+        let urlString = "\(weatherURL)lat=\(lat)&lon=\(lon)&appid=\(apiKey)"
+        print(urlString)
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    delegate?.didFailWithError(error: error!)
+                    print(error!)
+                }
+                
+                if let safeData = data {
+                    if let weather = parseJSON(safeData) {
+                        delegate?.didUpdateWeather(self, weather: weather)
+                        print(weather)
+                    }
+                }
+            }
+            task.resume()
+        }
         
+    }
+    
+    func parseJSON(_ weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            print(weather
+                .temperatureString)
+            return weather
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
     }
 }
