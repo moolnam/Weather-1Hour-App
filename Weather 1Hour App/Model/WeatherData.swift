9964d79c20cccbf9b066097a121425beff910689
moolnam <moolnam>
2022-05-03 15:13:42 +0900

    dd

--- Weather 1Hour App/Model/WeatherData.swift
+++ Weather 1Hour App/Model/WeatherData.swift
@@ -7,40 +7,17 @@
 
 import Foundation
 
-struct WeatherData {
-    let conditionId: Int
-    
-    
-    var conditionName: String {
-        switch conditionId {
-        case 200...232:
-            return "cloud.bolt.fill"
-        case 300...321:
-            return "cloud.drizzle.fill"
-        case 500...531:
-            return "cloud.rain.fill"
-        case 600...622:
-            return "cloud.snow.fill"
-        case 701...711:
-            return "smoke.fill"
-        case 721:
-            return "sun.haze.fill"
-        case 731:
-            return "sun.dust.fill"
-        case 741...751:
-            return "cloud.fog.fill"
-        case 761:
-            return "sun.dust.fill"
-        case 762:
-            return "cloud.fog.fill"
-        case 771...781:
-            return "tornado"
-        case 800:
-            return "sun.max"
-        default:
-            return "cloud.fill"
-        }
-    }
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
 }
 
+struct Main: Codable {
+    let temp: Double
+}
 
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
